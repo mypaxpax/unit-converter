be47e878708bec9bb031d52196b27558c555f724
mypaxpax <58120765+mypaxpax@users.noreply.github.com>
2023-01-21 14:04:54 +0100

    rename

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+const inputVal = document.querySelector("input-el");
+const convertBtn = document.getElementById("convert-btn");
+
+convertBtn.addEventListener("click", sayHi);
+
+function sayHi() {
+  console.log("hi");
+}
